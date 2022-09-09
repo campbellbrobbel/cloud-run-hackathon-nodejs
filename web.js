81de2719360c463724b18f6b375094298ad03e8c
Campbell Brobbel <campbellbrobbel@hotmail.com>
2022-09-09 11:56:24 +1000

    Basic logic

--- web.js
+++ web.js
@@ -39,9 +39,125 @@ app.post('/', function (req, res) {
 
 app.listen(process.env.PORT || 8080);
 
+const _ = require('lodash');
 
 const calculateValueForMove = (move, arena) => {
   let {dims, state} = arena
-  let myState = state[MY_URL]
-  return 100
+  let newState = _.cloneDeep(state)
+  applyMove(newState, move)
+  let player = state[MY_URL]
+  let nearestEnemy = null
+  let nearestEnemyDistance = null
+  let numberOfHitEnemies = 0
+  let enemyDetails = {
+
+  }
+  for(let enemy in newState) {
+    if(enemy !== MY_URL) {
+      let enemyDistance = calculateDistanceBetweenPlayers(player, newState[enemy])
+      let enemyDirection = calculateDirectionToEnemy(player, newState[enemy])
+      if(!nearestEnemyDistance) {
+        nearestEnemyDistance = enemyDistance
+      }
+      if(newState[enemy].wasHit) {
+        numberOfHitEnemies++
+      }
+    }
+  }
+  return (nearestEnemyDistance ) + (numberOfHitEnemies * 10)
+}
+
+
+const calculateDistanceBetweenPlayers = (player, enemy) => {
+  let {playerX, playerY } = player
+  let {enemyX, enemyY } = enemy
+  return Math.abs(playerX - enemyX) + Math.abs(playerY - enemyY)
+}
+
+const calculateDirectionToEnemy = (player, enemy) => {
+  let {playerX, playerY } = player
+  let {enemyX, enemyY } = enemy
+  let verticalDirection = null
+  if(playerY < enemyY) {
+    verticalDirection = ' N'
+  } else if(playerY > enemyY) {
+    verticalDirection = ' S'
+  }
+  let horizontalDirection = playerX < enemyX ? 'E' : 'W'
+  if(playerX < enemyX) {
+    horizontalDirection = 'E'
+  } else if(playerX > enemyX) {
+    horizontalDirection = 'W'
+  }
+
+  let directionString = ''
+  if(verticalDirection)
+    directionString += verticalDirection
+  if(horizontalDirection)
+    directionString += horizontalDirection
+
+  return directionString
+
+}
+
+const applyMove = (state, move) => {
+  let player = state[MY_URL]
+
+  switch(move) {
+    case 'F':
+      let x = 0
+      let y = 0
+      if(player.direction === Directions.N)
+        y += 1
+      else if(player.direction === Directions.S)
+        y -= 1
+      else if(player.direction === Directions.E)
+        x += 1
+      else if(player.direction === Directions.W)
+        x -= 1
+      state[MY_URL].x += x
+      state[MY_URL].y += y
+      break
+    case 'T':
+      // if(player.direction === Directions.N)
+      //   state[MY_URL].direction = Directions.W
+      // else if(player.direction === Directions.W)
+      //   state[MY_URL].direction = Directions.S
+      // else if(player.direction === Directions.S)
+      //   state[MY_URL].direction = Directions.E
+      // else if(player.direction === Directions.E)
+      //   state[MY_URL].direction = Directions.N
+      break
+    case 'L':
+      if(player.direction === Directions.N)
+        state[MY_URL].direction = Directions.W
+      else if(player.direction === Directions.W)
+        state[MY_URL].direction = Directions.S
+      else if(player.direction === Directions.S)
+        state[MY_URL].direction = Directions.E
+      else if(player.direction === Directions.E)
+        state[MY_URL].direction = Directions.N
+      break
+    case 'R':
+      if(player.direction === Directions.N)
+        state[MY_URL].direction = Directions.E
+      else if(player.direction === Directions.W)
+        state[MY_URL].direction = Directions.N
+      else if(player.direction === Directions.S)
+        state[MY_URL].direction = Directions.W
+      else if(player.direction === Directions.E)
+        state[MY_URL].direction = Directions.S
+      break
+  }
+}
+
+const Directions = {
+  N: 'N',
+  S: 'S',
+  E: 'E',
+  W: 'W',
+  NW: 'NW',
+  NE: 'NE',
+  SW: 'SW',
+  SE: 'SE'
 }
